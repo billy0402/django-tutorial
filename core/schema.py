f9a4a5a7dcdcf78a12a1cc4a75b3e50ebbc092af
Billy Huang <10646003@ntub.edu.tw>
2021-02-17 03:41:10 +0800

    feat(ingredients): Relay tutorial

--- core/schema.py
+++ core/schema.py
@@ -1,34 +1,12 @@
 import graphene
-from graphene_django import DjangoObjectType
 
-from ingredients.models import Category, Ingredient
-
-
-class CategoryType(DjangoObjectType):
-    class Meta:
-        model = Category
-        fields = ('id', 'name', 'ingredients')
+from ingredients import schema as ingredient_schema
 
 
-class IngredientType(DjangoObjectType):
-    class Meta:
-        model = Ingredient
-        fields = ('id', 'name', 'note', 'category')
-
-
-class Query(graphene.ObjectType):
-    all_ingredients = graphene.List(IngredientType)
-    category_by_name = graphene.Field(CategoryType, name=graphene.String(required=True))
-
-    def resolve_all_ingredients(root, info):
-        # We can easily optimize query count in the resolve method
-        return Ingredient.objects.select_related('category').all()
-
-    def resolve_category_by_name(root, info, name):
-        try:
-            return Category.objects.get(name=name)
-        except Category.DoesNotExist:
-            return None
+class Query(ingredient_schema.Query, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
 
 
 schema = graphene.Schema(query=Query)
