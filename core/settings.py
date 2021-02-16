c10486980b56887fe48a219816952f5e45186caa
Billy Huang <10646003@ntub.edu.tw>
2021-02-17 00:41:22 +0800

    feat(snippets): Tutorial 1 - Serialization

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'polls.apps.PollsConfig',
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
